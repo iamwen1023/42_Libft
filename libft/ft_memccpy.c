1a5914a3615a8fb9b049c2dd461265340a3198a3
iamwen1023 <60364538+iamwen1023@users.noreply.github.com>
2021-05-24 17:05:56 +0200

    Update ft_memccpy.c

--- libft/ft_memccpy.c
+++ libft/ft_memccpy.c
@@ -33,3 +33,37 @@ void	*ft_memccpy(void *dest, const void *src, int c, size_t n)
 	*ptr_dest++ = *ptr_src++;
 	return ((void *)ptr_dest);
 }
+
+
+int		check_c(const unsigned char *src, int c)
+{
+	while (*src)
+	{
+		if (*src == c)
+			return (1);
+		src++;
+	}
+	return (0);
+}
+
+void		*ft_memccpy(void *dest, const void *src, int c, size_t n)
+{
+	unsigned char	*ch_dest;
+	unsigned char	*ch_src;
+	size_t	i;
+
+	ch_src = (unsigned char*)src;
+	ch_dest = (unsigned char*)dest;
+	if (ch_src == 0 && ch_dest == 0)
+		return (0);
+	i = 0;
+	while (i < n && ch_src[i] != (unsigned char )c)
+	{
+		ch_dest[i] = ch_src[i];
+		i++;
+	}
+	if (i >= n && ch_src[i] != (unsigned char)c)
+		return (0);
+	ch_dest[i] = ch_src[i];
+	return (dest);
+}
