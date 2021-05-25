fbd9973a3544a5dec91610ef8ea22c094a5a2294
iamwen1023 <60364538+iamwen1023@users.noreply.github.com>
2021-05-25 12:15:08 +0200

    Update ft_memccpy.c

--- libft/ft_memccpy.c
+++ libft/ft_memccpy.c
@@ -23,47 +23,21 @@ void	*ft_memccpy(void *dest, const void *src, int c, size_t n)
 	if (ptr_dest == 0 && ptr_src == 0)
 		return (0);
 	i = 0;
-	while (i < n && *ptr_src != (unsigned char)c)
+	//while (i < n && *ptr_src != (unsigned char)c)
+	//{
+	//	*ptr_dest++ = *ptr_src++;
+	//	i++;
+	//}
+	//if (i >= n && *ptr_src != (unsigned char)c)
+	//	return (0);
+	//*ptr_dest++ = *ptr_src++;
+	//return ((void *)ptr_dest);
+	while (i < n)
 	{
-		*ptr_dest++ = *ptr_src++;
+		ptr_dest[i] = ptr_src[i];
+		if (ptr_src[i] == (unsigned char)c)
+				return (dest + i + 1);
 		i++;
-	}
-	if (i >= n && *ptr_src != (unsigned char)c)
-		return (0);
-	*ptr_dest++ = *ptr_src++;
-	return ((void *)ptr_dest);
-}
-
-
-int		check_c(const unsigned char *src, int c)
-{
-	while (*src)
-	{
-		if (*src == c)
-			return (1);
-		src++;
 	}
 	return (0);
-}
-
-void		*ft_memccpy(void *dest, const void *src, int c, size_t n)
-{
-	unsigned char	*ch_dest;
-	unsigned char	*ch_src;
-	size_t	i;
-
-	ch_src = (unsigned char*)src;
-	ch_dest = (unsigned char*)dest;
-	if (ch_src == 0 && ch_dest == 0)
-		return (0);
-	i = 0;
-	while (i < n && ch_src[i] != (unsigned char )c)
-	{
-		ch_dest[i] = ch_src[i];
-		i++;
-	}
-	if (i >= n && ch_src[i] != (unsigned char)c)
-		return (0);
-	ch_dest[i] = ch_src[i];
-	return (dest);
 }
