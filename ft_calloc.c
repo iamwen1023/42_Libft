75b80f165ec964a07bcae5874bf8594fcc5fbc04
Wen-tzu LO <wlo@e2r10p16.42.fr>
2021-05-28 17:03:22 +0200

    retry

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: wlo <marvin@42.fr>                         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/20 12:52:55 by wlo               #+#    #+#             */
-/*   Updated: 2021/05/25 10:14:12 by wlo              ###   ########.fr       */
+/*   Updated: 2021/05/27 17:30:24 by wlo              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,9 @@ void	*ft_calloc(size_t nmemb, size_t size)
 {
 	void	*arr;
 
-	if (nmemb == 0 || size == 0)
-	{
-		arr = malloc(sizeof(0));
-		if (!arr)
-			return (0);
-	}
 	arr = malloc(nmemb * size);
 	if (!arr)
 		return (0);
-	ft_bzero(arr, nmemb * size);
+	ft_bzero(arr, (nmemb * size));
 	return (arr);
 }
--- ft_strtrim.c
+++ ft_strtrim.c
@@ -11,7 +11,6 @@
 /* ************************************************************************** */
 
 #include "libft.h"
-#include "stdio.h"
 
 int	if_set(char c, char const *set)
 {
